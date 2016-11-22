fe21adcb1dc0eda61656fa7d96d86b595419096c
Oleg-Savicki <katexizis94@gmail.com>
2016-11-22 12:42:31 +0300

    Add files via upload

---
+++ TestModules/ByeProject/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by katex on 22.11.2016.
+ */
+
+var byeDisplay;
+
+byeDisplay.sayBye = function () {
+  console.log('Bye!');
+};
+
+module.exports = byeDisplay;
---
+++ TestModules/HelloProject/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by katex on 22.11.2016.
+ */
+
+var helloDispaly;
+
+helloDispaly.sayHello = function () {
+  console.log("Hello!");
+};
+
+module.exports = helloDispaly;
